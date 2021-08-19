2e81dcb36c1ff7a66e597115e842133dc54e1d53
Andrew Carrigan <carrigan223@gmail.com>
2021-08-18 17:38:52 -0700

    added layout page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,31 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout')
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
+<h1>The About Page goes here</h1>
+@stop
 
-        <title>About</title>
+@section('footer')
+<script>
+    const message = "this is the about page";
+        alert(message);
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
-        </style>
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-
-    <body>
-        <div>
-            <h1>Hello About Page</h1>
-        </div>
-    </body>
-
-</html>
+</script>
+@stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout')
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-
-    <body>
-
-        @unless (empty($people))
-        There Are No People
-        @else
-        These are the People
-        @endunless
-
-        @foreach ($people as $person)
-        <li>{{ $person }}</li>
-        @endforeach
-    </body>
-
-</html>
+@section('content')
+<h1>Welcome Page Here</h1>
+@stop
